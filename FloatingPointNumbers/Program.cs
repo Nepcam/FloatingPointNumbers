e0de3203a54a21703d4cae27bafec7bac1757653
Nepcam <cameron.nepe@gmail.com>
2020-03-23 21:17:51 +1300

    variable assignment

--- FloatingPointNumbers/Program.cs
+++ FloatingPointNumbers/Program.cs
@@ -10,6 +10,14 @@ namespace FloatingPointNumbers
     {
         static void Main(string[] args)
         {
+            float f_no = 0.0f;
+            double d_no = 0.0d;
+            decimal m_no = 0.0m;
+
+            // Place holder
+            Console.WriteLine("Float no. is {0}", f_no);
+            Console.WriteLine("Double no. is {0}", d_no);
+            Console.WriteLine("Decimal no. is {0}", m_no);
         }
     }
 }
