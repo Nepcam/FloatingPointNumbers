4cf39ba7d0cf7ec4cc7fefac103ccf506a116d77
Nepcam <cameron.nepe@gmail.com>
2020-03-23 21:24:44 +1300

    assign values to float double decimal variables and run

--- FloatingPointNumbers/Program.cs
+++ FloatingPointNumbers/Program.cs
@@ -14,6 +14,10 @@ namespace FloatingPointNumbers
             double d_no = 0.0d;
             decimal m_no = 0.0m;
 
+            f_no = 1 / 3f;
+            d_no = 1 / 3d;
+            m_no = 1 / 3m;
+
             // Place holder
             Console.WriteLine("Float no. is {0}", f_no);
             Console.WriteLine("Double no. is {0}", d_no);
